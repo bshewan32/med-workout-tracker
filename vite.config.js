2cce66fe1f7ed3e91f198299cf5cbf1923cb04b7
bshewan32 <bill.shewan@icloud.com>
2026-01-05 02:27:32 +0000

    first big commit

--- vite.config.js
+++ vite.config.js
@@ -1,1 +1,9 @@
+import { defineConfig } from "vite";
 
+export default defineConfig({
+  // If you're deploying at a subpath, set base: "/subpath/"
+  // base: "/",
+  build: {
+    outDir: "dist",
+  },
+});
